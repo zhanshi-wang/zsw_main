5ea70d9af1855dad7f3a249e0af04428608f0c07
Zhanshi Wang <zhanshi.wang.verdandi@gmail.com>
2022-03-06 09:35:47 -0800

    complex_fib

--- complex_fib/client/src/App.js
+++ complex_fib/client/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
@@ -9,12 +9,11 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
           <div>
             <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={Otherpage} />
+            <Route path="/otherpage" component={OtherPage} />
           </div>
         </header>
       </div>
--- complex_fib/client/src/OtherPage.js
+++ complex_fib/client/src/OtherPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => {
+function OtherPage() {
     return (
         <div>
             I'm some other page!
             <Link to="/">Go back home</Link>
         </div>
     )
-}
+};
+
+export default OtherPage
